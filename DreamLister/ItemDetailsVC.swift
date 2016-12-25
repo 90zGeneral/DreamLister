4d57a2bbfb0def57546aa364b1b06c0dd0bee6cd
Roydon Jeffrey <brushdam14823@gmail.com>
2016-12-25 16:11:37 -0500

    Designing the ItemDetailsVC to add a new item or modify an existing item. Segues are made from the MainVC itself and the '+' button

---
+++ DreamLister/ItemDetailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  ItemDetailsVC.swift
+//  DreamLister
+//
+//  Created by Roydon Jeffrey on 12/25/16.
+//  Copyright © 2016 Italyte. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
